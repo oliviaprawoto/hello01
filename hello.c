40bb92caba331e6903d1789ab9e1f432fa39a589
Olivia Prawoto <oprawoto2@huskers.unl.edu>
2019-09-09 11:03:30 -0500

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int arfc, char **argv) {
+
+  printf("Olivia Glock\n") ;
+  printf("Marketing\n") ;
+  printf("Olivia Prawoto\n") ;
+  printf("Computer Science\n") ;
+  return 0;
+}
